bd633f9e0e9c268d8f53d3149af29895e2903b2f
jdcruzc1 <jdcruzc1@gmail.com>
2022-10-09 11:49:09 -0500

    Se agrega el proyecto

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(300, 300);
+    background (100,100,255);
+    let grosor = 1;
+
+    for(let i = 0; i < width; i = i + 10){ 
+    stroke(0, 0, 0);
+    strokeWeight(grosor);
+   line (i, 0, i, 300);
+
+   
+   strokeWeight(1);
+   stroke(0, 0, 0);
+   line (0, i, 300, i);
+   grosor ++;
+   console.log(grosor);
+console.log(i);
+    }
+
+  
   }
   
   function draw() {
-    background(220);
-    noStroke();
-    fill(0, 255, 0);
-    ellipse(200, 200, 100, 100);
-    fill(0);
-    textSize(40);
-    textAlign(CENTER, CENTER);
-    text(':-)', 200, 200);
-  }
+    
+  
+    }
+    
+  
+ 
